29779d60539d12ebd32da7f79d04d680ea6c2e87
Tyrell Williams <tyrellwilliams2020@gmail.com>
2020-09-23 14:48:04 -0400

    finished product

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = 'Tyrell Williams'
 
 import re
 import os
@@ -19,18 +19,30 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    paths = os.listdir(dirname)
+    special_paths = []
+    for filename in paths:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            special_paths.append(
+                os.path.abspath(os.path.join(dirname, filename)))
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
-    return
+    """copies files to dest_dir """
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        filename = os.path.basename(path)
+        shutil.copy(path, os.path.join(dest_dir, filename))
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
-    return
+    """zip files to dest_zip"""
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    subprocess.check_output(cmd)
 
 
 def main(args):
@@ -39,6 +51,8 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('from_dir', help='search directory')
+
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
 
@@ -51,6 +65,14 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
+
+    special_paths = get_special_paths(ns.from_dir)
+    if ns.todir:
+        copy_to(special_paths, ns.todir)
+    elif ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        print('\n'.join(special_paths))
 
 
 if __name__ == "__main__":
